537517c554aeb5d8913013284124fd669d62f9db
putra299 <syurautamap84@gmail.com>
2022-07-06 13:51:22 +0700

    devops test

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+
+import ( 
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	os.Setenv("PORT", "8080")
+
+	port := os.Getenv("PORT")
+	
+		if port == "" {
+			log.Fatal("PORT env is required")
+		}
+		
+	instanceID := os.Getenv("INSTANCE_ID")
+	
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		if r.Method != "GET" {
+			http.Error(w, "http method not allowed", http.StatusBadRequest)
+			return
+		}
+		
+	text := "Hello world"
+    	if instanceID != "" {
+        	text = text + ". From " + instanceID
+    	}
+
+    	w.Write([]byte(text))
+    	})
+    	
+    	server := new(http.Server)
+	server.Handler = mux
+	server.Addr = "0.0.0.0:" + port
+
+	log.Println("server starting at", server.Addr)
+	err := server.ListenAndServe()
+	
+	if err != nil {
+    		log.Fatal(err.Error())
+
+	}
+}

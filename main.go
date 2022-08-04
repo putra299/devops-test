f08e7bcac8770595be1b5f704e5da0301f1fd0f6
putra299 <syurautamap84@gmail.com>
2022-08-04 11:01:56 +0700

    coba

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 			return
 		}
 
-		text := "Hello Putra Test Lagi"
+		text := "Hello Putra Test"
 		if instanceID != "" {
 			text = text + " " + "APP " + os.Getenv("APPLICATION") + " " + "Versi: " + os.Getenv("VERSION")
 		}
